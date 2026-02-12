44045e8fa45c97e62dbbef35a312802abe482e02
Amena-jlassi <Amena.Jlassi@esprit.tn>
2026-02-12 13:08:44 +0100

    Activities_microservice

---
+++ Activities_service/src/main/java/tn/esprit/activities_service/ActivitiesServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.activities_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiesServiceApplication.class, args);
+    }
+
+}
